d9c0b32cacc2b1759c842bdc45ef0ba66bef0786
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-09-15 18:07:09 +0400

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Homework02 from './homeworks/Homework_02/Homework_02';
+import Lesson_02 from './lessons/Lesson_02/Lesson_02';
+
 
 function App() {
   return (
     <div className="App">
       My first React component
       <p>John Johnson</p>
+      <Homework02 />
       </div>
   );
 }
