36696ffd031d11301230699f49fcee69dce60a9e
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-09-17 23:31:32 +0400

    add lesson_03

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-// LESSONS IMPORT
+// LESSONS
+// 3 шаг - импортироуем компонент Lesson_02
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 
-import Lessons_02 from './lessons/Lesson_02/Lesson_02'
+// HOMEWORKS
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
 
-// HOMEWORKS IMPORT
-
-
-// CONSULTATIONS IMPORT
+// CONSULTATIONS
 
 import "./App.css";
-import Homework02 from "./homeworks/Homework_02/Homework_02";
+
+// Правила компонентов:
+// 1. Названия пишем с большой буквы
+// 2. Компонент - это функция и она должна возрващать JSX
+// 3. JSX - синтаксический сахар, позволяющий возращать HTML элементы из компонента, а также прописывать логику прямо в HTML
 
 function App() {
   return (
     <div className="App">
-      My first React component
-      <p>John Johnson</p>
-      {/* LESSONS */}
+      {/* Lessons */}
+      {/* 4 шаг - возвращаем компонент Lesson_02 из компонента App */}
       {/* <Lesson_02 /> */}
-      {/* HOMEWORKS */}
-      <Homework02 />
-      {/* CONSULTAIONS */}
+      <Lesson_03 />
+      {/* Homeworks */}
+      {/* <Homework_02 /> */}
     </div>
   );
 }
